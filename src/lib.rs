ebd411099345685469c5fb4928685dbed943a1b4
Rein van der Woerd <reinvanderwoerd@me.com>
2020-04-26 23:26:46 +0200

    Dry/wet mix

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,21 @@
 #[macro_use] extern crate vst;
 
+extern crate lerp;
+
 use vst::buffer::AudioBuffer;
 use vst::plugin::{Plugin, Info};
-
+use lerp::Lerp;
 
 struct DigiDist {
-    threshold: f32
+    threshold: f32,
+    dry_wet_mix: f32
 }
 
 impl Default for DigiDist{
     fn default() -> Self {
         DigiDist {
-            threshold: 1.0
+            threshold: 1.0,
+            dry_wet_mix: 0.0
         }
     }
 }
@@ -26,7 +30,7 @@ impl Plugin for DigiDist {
 
             inputs: 2,
             outputs: 2,
-            parameters: 1,
+            parameters: 2,
 
             ..Info::default()
         }
@@ -35,6 +39,7 @@ impl Plugin for DigiDist {
     fn get_parameter_label(&self, index: i32) -> String {
         match index {
             0 => "%".to_string(),
+            1 => "%".to_string(),
             _ => "".to_string(),
         }
     }
@@ -42,6 +47,7 @@ impl Plugin for DigiDist {
     fn get_parameter_text(&self, index: i32) -> String {
         match index {
             0 => format!("{}", self.threshold * 100.0),
+            1 => format!("{}", self.dry_wet_mix * 100.0),
             _ => "".to_string(),
         }
     }
@@ -49,6 +55,7 @@ impl Plugin for DigiDist {
     fn get_parameter_name(&self, index: i32) -> String {
         match index {
             0 => "Threshold".to_string(),
+            1 => "Dry/Wet Mix".to_string(),
             _ => "".to_string(),
         }
 
@@ -57,6 +64,7 @@ impl Plugin for DigiDist {
     fn get_parameter(&self, index: i32) -> f32 {
         match index {
             0 => self.threshold,
+            1 => self.dry_wet_mix,
             _ => 0.0,
         }
     }
@@ -64,6 +72,7 @@ impl Plugin for DigiDist {
     fn set_parameter(&mut self, index: i32, value: f32) {
         match index {
             0 => self.threshold = value.max(0.01),
+            1 => self.dry_wet_mix = value,
             _ => (),
         };
     }
@@ -72,11 +81,15 @@ impl Plugin for DigiDist {
         for (input, output) in buffer.zip() {
             // For each input sample and output sample in buffer
             for (in_frame, out_frame) in input.into_iter().zip(output.into_iter()) {
+                let distorted;
+
                 if *in_frame >= 0.0 {
-                    *out_frame = in_frame.min(self.threshold) / self.threshold;
+                    distorted = in_frame.min(self.threshold) / self.threshold;
                 } else {
-                    *out_frame = in_frame.max(-self.threshold) / self.threshold;
+                    distorted = in_frame.max(-self.threshold) / self.threshold;
                 }
+
+                *out_frame = in_frame.lerp(distorted, self.dry_wet_mix);
             }
         }
     }
